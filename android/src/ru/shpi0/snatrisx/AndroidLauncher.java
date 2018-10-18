480ddf41746fcfddc5b58a097bae390ed8a4e966
Vladimir Yarovoy <shpi0@yandex.ru>
2018-10-18 22:06:26 +0300

    initial commit

---
+++ android/src/ru/shpi0/snatrisx/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.shpi0.snatrisx;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.shpi0.snatrisx.SnatrisX;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SnatrisX(), config);
+	}
+}
---
+++ desktop/src/ru/shpi0/snatrisx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.shpi0.snatrisx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.shpi0.snatrisx.SnatrisX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnatrisX(), config);
+	}
+}
