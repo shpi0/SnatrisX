c18db22130e8248501d1c133a0e9b1254b8facff
Vladimir Yarovoy <shpi0@yandex.ru>
2018-10-19 21:44:12 +0300

    Moving and blinking logo

--- core/src/ru/shpi0/snatrisx/SnatrisX.java
+++ core/src/ru/shpi0/snatrisx/SnatrisX.java
@@ -8,8 +8,72 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SnatrisX extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture img = null;
+
+	int width = 0;
+	int height = 0;
+	int posX = 0;
+	int posY = 0;
+	float a = 0f;
+
+	public SnatrisX() {
+		final Thread logoColoringThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				boolean growUp = true;
+				while (true) {
+					if (growUp) {
+						a += 0.006f;
+					} else {
+						a -= 0.006f;
+					}
+					if (a >= 1f) {
+						growUp = false;
+					}
+					if (a <= 0.007f) {
+						growUp = true;
+					}
+					try {
+						Thread.sleep(16);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		logoColoringThread.setDaemon(true);
+		logoColoringThread.start();
+
+		Thread moveThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int x = 1;
+				int y = 1;
+				while (true) {
+					if (img != null) {
+						width = Gdx.graphics.getWidth();
+						height = Gdx.graphics.getHeight();
+						if (img.getHeight() + posY > height || posY < 0) {
+							y *= -1;
+						}
+						if (img.getWidth() + posX > width || posX < 0) {
+							x *= -1;
+						}
+						posX += x;
+						posY += y;
+					}
+					try {
+						Thread.sleep(16);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		moveThread.setDaemon(true);
+		moveThread.start();
+	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -21,7 +85,8 @@ public class SnatrisX extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.setColor(1, 1, 1, a);
+		batch.draw(img, posX, posY);
 		batch.end();
 	}
 	
